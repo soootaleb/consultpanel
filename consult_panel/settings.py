bea1b9a521b2ef06be6f931b26339538241312c5
Sofiane Taleb <sofiane.taleb.94@gmail.com>
2016-03-05 03:18:58 +0100

    fixed allowed hosts

--- consult_panel/settings.py
+++ consult_panel/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'or_9e-x+xji0k8p6f@z02r%v#d8&c*2z+w+46)!s-x!4#y0am6'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['www.rqms.fr', 'rqms.fr', 'http://46.101.62.246/']
 
 # Application definition
 
