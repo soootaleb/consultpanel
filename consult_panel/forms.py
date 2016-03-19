3584ac56c28ec1d1f62b255d9366fb8bb137ac5e
Quentin Bross <quentinb8@gmail.com>
2016-03-19 13:41:43 +0100

    Login page HTML

--- consult_panel/forms.py
+++ consult_panel/forms.py
@@ -1,5 +1,17 @@
 from django import forms
 
 class LoginForm(forms.Form):
-    username = forms.CharField(label='Username', max_length=100)
-    password = forms.CharField(widget=forms.PasswordInput())
+    username = forms.CharField(
+        label='Username',
+        max_length=100,
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Nom'
+        })
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Mot de Passe'
+        })
+    )
