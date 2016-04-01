114a21b6983b8a28b6b1993d975bd2739790c48d
matthieuberger <matthieu.berger@etu.unice.fr>
2016-04-01 20:37:23 +0200

    Entreprise Model added

--- consult_panel/models.py
+++ consult_panel/models.py
@@ -3,11 +3,26 @@ from django.contrib.auth.models import User
 
 
 
+class Entreprise(models.Model):
+    nom = models.CharField(max_length=200)
+    nom_commercial = models.CharField(max_length=200, blank=True)
+    siren = models.CharField(max_length=8, unique=True)
+    siret = models.CharField(max_length=14, unique=True)
+    adresse = models.CharField(max_length=250)
+    code_postal = models.CharField(max_length=10)
+    telephone = models.CharField(max_length=15)
+    
+    def __str__(self):
+        if self.nom_commercial:
+            return self.nom_commercial;
+        else:
+            return self.nom;
+        
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    entreprise = models.ForeignKey(Entreprise)
     biography = models.CharField(max_length=200)
-    
     def __str__(self):
         return self.first_name + " " + self.last_name;
     
