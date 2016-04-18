98c5a138f46d86e9993dcb9eea83bf1d800343b3
matthieuberger <matthieu.berger@etu.unice.fr>
2016-04-18 21:08:31 +0200

    corrected bug custom form for catalog and formation

--- admin_panel/forms.py
+++ admin_panel/forms.py
@@ -1,6 +1,7 @@
 from consult_panel.models import *
 from django.forms import *
 from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
 
 class FormationForm(ModelForm):
 
@@ -8,6 +9,7 @@ class FormationForm(ModelForm):
         self.helper = FormHelper()
         self.helper.label_class = 'col-sm-2'
         self.helper.field_class = 'col-sm-10'
+        self.helper.add_input(Submit('submit', 'Sauvegarder'))
         super(FormationForm, self).__init__(*args, **kwargs)
 
     class Meta:
@@ -24,6 +26,7 @@ class CatalogueForm(ModelForm):
         self.helper = FormHelper()
         self.helper.label_class = 'col-sm-2'
         self.helper.field_class = 'col-sm-10'
+        self.helper.add_input(Submit('submit', 'Sauvegarder'))
         super(CatalogueForm, self).__init__(*args, **kwargs)
 
     class Meta:
