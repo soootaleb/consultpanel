ac9bd9980ac97daa9a71094d39328fb5bb392e12
Sofiane Taleb <sofiane.taleb.94@gmail.com>
2016-04-26 00:25:46 +0200

    added documents/index for landing page & update admin_root

--- admin_panel/urls.py
+++ admin_panel/urls.py
@@ -1,27 +1,14 @@
-"""consult_panel URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.9/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
 from django.conf.urls import url, include
 from django.contrib import admin
 
-from .views import admin_pages, admin_profile, admin_sessions, admin_formations, admin_catalogues
+from .views import admin_pages, admin_profile, admin_sessions, admin_formations, admin_catalogues, admin_documents
 
 urlpatterns = [
     url(r'^$', admin_pages.index, name='admin_index'),
     url(r'^index/$', admin_pages.index, name='admin_index'),
     url(r'^profile/$', admin_profile.index, name='profile_index'),
+    url(r'^documents/$', admin_documents.documents_index, name='documents_index'),
+    url(r'^documents/index/$', admin_documents.documents_index, name='documents_index'),
     url(r'^sessions/$', admin_sessions.sessions_index, name='sessions_index'),
     url(r'^sessions/index/$', admin_sessions.sessions_index, name='sessions_index'),
     url(r'^sessions/add/$', admin_sessions.sessions_add, name='sessions_add'),
