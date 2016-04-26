ad4c55bb58b4c0dcf8421e31efae6cae76e66d0c
Quentin Bross <quentinb8@gmail.com>
2016-04-27 00:09:44 +0200

    Index calendar

---
+++ consult_panel/static/main/js/lib/fullcalendar/lang/fr.js
@@ -0,0 +1,11 @@
+$.fullCalendar.lang("fr", {
+	buttonText: {
+		today: "Aujourd'hui",
+		month: "Mois",
+		week: "Semaine",
+		day: "Jour",
+		list: "Mon planning"
+	},
+	allDayHtml: "Toute la<br/>journée",
+	eventLimitText: "en plus"
+});
