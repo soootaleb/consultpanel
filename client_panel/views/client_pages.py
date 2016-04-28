5895fd6abb4c22161d04b044e06397f9d605b41c
Sofiane Taleb <sofiane.taleb.94@gmail.com>
2016-04-28 18:32:24 +0200

    added client interface

--- client_panel/views/client_pages.py
+++ client_panel/views/client_pages.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'client_root.html')
+    return render(request, 'client_pages_index.html', context={
+        'page_title'        :   "Bienvenue sur votre espace client"
+    })
--- consult_panel/urls.py
+++ consult_panel/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     url(r'^api/', include('public_api.urls')),
     url(r'^ajax/', include('ajax_api.urls')),
     url(r'^master/', include('master_panel.urls')),
+    url(r'^client/', include('client_panel.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^consult/', include('admin_panel.urls')),
 ]
