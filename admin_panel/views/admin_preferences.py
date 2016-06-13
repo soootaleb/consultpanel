978dfea18a598ee2d34c0e9f9f372dde49207831
Sofiane Taleb <sofiane.taleb.94@gmail.com>
2016-06-13 14:41:26 +0200

    updated view to list real pdf files

--- admin_panel/views/admin_preferences.py
+++ admin_panel/views/admin_preferences.py
@@ -1,16 +1,22 @@
 from django.shortcuts import render
 from consult_panel.models import *
+from consult_panel.settings import *
 from django.contrib.auth.decorators import permission_required
 from document_generator.models import *
 from admin_panel.forms.forms import FileForm
 
+import os
+
 
 def index(request):
     #user = Profile.objects.get(user=request.user)
+    files = os.listdir(os.path.join(MEDIA_ROOT, 'admin_documents'))
+    pdf_ones = [x for x in files if x[-4:] == '.pdf']
     return render(request, 'admin_preferences_index.html', context={
         'user':   request.user,
         'page_title':   'Préférences',
         'form': FileForm(),
         'dg_types':   DocumentType.objects.all(),
-        'dg_templates':   Template.objects.all()
+        'dg_templates': pdf_ones
+        #'dg_templates':   Template.objects.all()
     })
