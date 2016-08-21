c7fea726dc18a4c51dceef8a3fc0a704f49b68e6
QuentinBrosse <quentinb8@gmail.com>
2016-08-21 20:24:19 +0200

    Remove e400 useless url

--- public_site/urls.py
+++ public_site/urls.py
@@ -8,9 +8,6 @@ urlpatterns = [
     url(r'^index/$', public_pages.public_index, name='public_index'),
     url(r'^login/', public_pages.login, name='public_login'),
     url(r'^logout/', public_pages.logout, name='public_logout'),
-
-    url(r'^404/', public_pages.e404, name='e404'),
-
     url(r'^register/', public_pages.RegistrationWizard.as_view([RegistrationForm, CentreFormationForm]), name='public_register'),
     url(r'^forgot-password', public_pages.forgot_password, name="public_forgot_password"),
     url(r'^form/(?P<name>[a-z_]+)', public_pages.form, name="public_form"),
