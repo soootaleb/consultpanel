0b4f2fcccbbe2bd03c723a4c6091aef75e3aef22
Sofiane Taleb <sofiane.taleb.94@gmail.com>
2017-04-03 09:50:36 +0200

    Added DocumentGeneratorConfig

---
+++ documents/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'public_site.apps.DocumentGeneratorConfig'
--- documents/apps.py
+++ documents/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class DocumentGeneratorConfig(AppConfig):
     name = 'documents'
+    verbose_name = 'Document generator'
