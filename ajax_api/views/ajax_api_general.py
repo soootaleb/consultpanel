873d780b4a7f56728fb887fc067e56f3fafa2fce
Sofiane Taleb <sofiane.taleb.94@gmail.com>
2018-04-08 18:12:29 +0200

    Dynamic AJAX entites count

--- ajax_api/views/ajax_api_general.py
+++ ajax_api/views/ajax_api_general.py
@@ -1,11 +1,12 @@
 from django.http import JsonResponse
+from consult_panel.models import *
 
 def get_entites_count(request):
     return JsonResponse({
-        'formations': 10,
-        'catalogues': 5,
-        'sessions': 0,
-        'centres': 19,
-        'entreprises': 1,
-        'clients': 12
+        'sessions': Session.objects.filter(formation__catalogue__profile__user=request.user).distinct().count(),
+        'formations': Formation.objects.filter(catalogue__profile__user=request.user).distinct().count(),
+        'clients': Client.objects.filter(catalogue__profile__user=request.user).distinct().count(),
+        'catalogues': Catalogue.objects.filter(profile__user=request.user).distinct().count(),
+        'entreprises': Profile.objects.get(user=request.user).liste_entreprises.count(),
+        'centres': Localisation.objects.filter(profile__user=request.user).distinct().count()
     })
