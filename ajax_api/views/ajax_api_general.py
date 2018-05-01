bf0a99f9251800bd1ff6ab555c9c89831d35279a
Sofiane Taleb <sofiane.taleb.94@gmail.com>
2018-05-01 18:54:34 +0200

    Fixed badges notification bug #54

--- ajax_api/views/ajax_api_general.py
+++ ajax_api/views/ajax_api_general.py
@@ -6,7 +6,7 @@ def get_entites_count(request):
         'sessions': Session.objects.filter(formation__catalogue__profile__user=request.user).distinct().count(),
         'formations': Formation.objects.filter(catalogue__profile__user=request.user).distinct().count(),
         'clients': Client.objects.filter(catalogue__profile__user=request.user).distinct().count(),
-        'catalogues': Catalogue.objects.filter(profile__user=request.user).distinct().count(),
+        'catalogues': Catalogue.objects.filter(profile__user=request.user).exclude(nom='main').distinct().count(),
         'entreprises': Profile.objects.get(user=request.user).liste_entreprises.count(),
         'centres': Localisation.objects.filter(profile__user=request.user).distinct().count()
     })
