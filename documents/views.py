340db9acd6eacc64be58f6fa59e1505a68336c49
Quentin Brosse <quentin.brosse@epitech.eu>
2018-05-01 22:45:07 +0200

    Start fixing convention signature

--- documents/views.py
+++ documents/views.py
@@ -1,11 +1,17 @@
 import jinja2
 import os
+import tempfile
+import base64
+import re
+import io
+
 from django.http import HttpResponse
 from documents.models import Convention
-from consult_panel.models import Profile, Cours, Inscription
+from consult_panel.models import Profile, Inscription
 from admin_panel import user_tests
 from django.contrib.auth.decorators import user_passes_test
-from docxtpl import DocxTemplate
+from docxtpl import DocxTemplate, InlineImage
+from docx.image.image import Image
 
 
 @user_passes_test(user_tests.is_formateur)
@@ -28,9 +34,20 @@ def convention_show(request, convention_id):
         convention.cours.get_date_debut()
     )
 
+    doc = DocxTemplate(template_path)
+
+    signature = None
+    tmp_img_file = None
+    if profile.signature_base64:
+        tmp_img_file = tempfile.NamedTemporaryFile(delete=None, suffix='.jpg')
+        img_blob = base64.urlsafe_b64decode(profile.signature_base64)
+        tmp_img_file.write(img_blob)
+        tmp_img_file.close()
+
+        signature = InlineImage(doc, tmp_img_file.name)
+
     jinja_env = jinja2.Environment(autoescape=True)
 
-    doc = DocxTemplate(template_path)
     context = {
         'convention': convention,
         'client': convention.client,
@@ -38,8 +55,13 @@ def convention_show(request, convention_id):
         'profile': profile,
         'centre_formation': profile.centre_formation,
         'inscriptions': inscriptions,
+        'signature': signature,
     }
+
     doc.render(context, jinja_env)
+
+    if tmp_img_file:
+        os.unlink(tmp_img_file.name)
 
     content_type = 'application/vnd.openxmlformats-officedocument'
     content_type += '.wordprocessingml.document'
@@ -48,6 +70,6 @@ def convention_show(request, convention_id):
         convention_title
     )
 
-    doc.get_docx().save(response)
+    doc.get_docx().save("/Users/quentinbrosse/Desktop/file.docx")
 
     return response
