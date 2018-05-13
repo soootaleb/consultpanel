b6959d4a4d9b9a99f30fbd2ebf8e069aeca9c96d
Quentin Brosse <quentin.brosse@epitech.eu>
2018-05-13 18:25:42 +0200

    Use SQLite for local env

--- consult_panel/settings/local.py
+++ consult_panel/settings/local.py
@@ -3,3 +3,10 @@ from .base import *
 DEBUG = True
 
 ALLOWED_HOSTS = ['localhost']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'cpanel_local.db',
+    }
+}
