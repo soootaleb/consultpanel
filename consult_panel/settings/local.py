c9c01891aabd8e6568a234199a0a93e1258ec94c
Quentin Brosse <quentin.brosse@epitech.eu>
2018-05-23 17:09:32 +0200

    Remove local DATABASES setting

--- consult_panel/settings/local.py
+++ consult_panel/settings/local.py
@@ -3,10 +3,3 @@ from .base import *
 DEBUG = True
 
 ALLOWED_HOSTS = ['localhost']
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'cpanel_local.db',
-    }
-}
