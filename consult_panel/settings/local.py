12224de626b73ab3387d6de69d61668488cf4913
Matthieu BERGER <berger@braincorporation.com>
2018-05-30 15:46:39 -0700

    Add localdb command to manage.py  #71

--- consult_panel/settings/local.py
+++ consult_panel/settings/local.py
@@ -7,7 +7,10 @@ ALLOWED_HOSTS = ['localhost']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'cpanel_local.db',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': '0.0.0.0',
+        'PORT': 5432
     }
 }
