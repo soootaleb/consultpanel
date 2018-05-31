1a9fb756480e0cae06b173ea30cba937b7de9287
Quentin Brosse <quentin.brosse@icloud.com>
2018-05-31 15:09:52 +0200

    Add docker to Pipfile + Remove databases local config

--- consult_panel/settings/local.py
+++ consult_panel/settings/local.py
@@ -2,17 +2,7 @@ from .base import *
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost']
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'HOST': '0.0.0.0',
-        'PORT': 5432
-    }
-}
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 UNIQUE_LINKER_BASE_URL = "http://localhost:8000"
 CONSOLE_MAILER = True
